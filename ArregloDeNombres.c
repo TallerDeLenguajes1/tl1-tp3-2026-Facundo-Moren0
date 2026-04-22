27a3cbabfdafe1b8efb876d7c12323b82a603441
Facundo-Moren0 <FacundoMoreno451@gmail.com>
2026-04-22 12:41:27 -0300

    Creacion de la funcion BuscarNombre

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -5,6 +5,8 @@
 #define CANTIDAD 5
 
 void MostrarPersonas(char *arrayNombres[],int cant);
+void BuscarNombre(char *arrayNombres[],int cant,int id);
+
 
 int main(){
     char nombre[50];
@@ -33,3 +35,15 @@ void MostrarPersonas(char *arrayNombres[],int cant){
     }
     
 }
+
+void BuscarNombre(char *arrayNombres[],int cant,int id){
+    
+    if(id >= cant || id < 0){
+        printf("No se encontró el valor buscado");
+    }else{
+        printf("%s",arrayNombres[id]);
+        printf("\n");
+    }
+    
+}
+
